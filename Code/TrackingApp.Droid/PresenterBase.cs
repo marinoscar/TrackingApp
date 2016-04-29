dd5b726d29ee0a16ed8d8903705ea599b3d99755
Oscar Marin <oscar@marin.cr>
2016-04-28 20:13:42 -0600

    change the signature of the method

--- Code/TrackingApp.Droid/PresenterBase.cs
+++ Code/TrackingApp.Droid/PresenterBase.cs
@@ -12,13 +12,20 @@ using Android.Widget;
 
 namespace TrackingApp.Droid
 {
-    public class PresenterBase
+    public abstract class PresenterBase
     {
         public PresenterBase(IActivity activity)
         {
             Activity = activity;
+            BindView();
         }
 
         public virtual IActivity Activity { get; private set; }
+
+        /// <summary>
+        /// Bind the view events to the activity
+        /// </summary>
+        public abstract void BindView();
+
     }
 }
