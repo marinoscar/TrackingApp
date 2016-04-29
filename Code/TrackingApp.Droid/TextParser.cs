db44d27e50b6199c5665e41ade0d5969d02d4881
Oscar Marin <oscar@marin.cr>
2016-04-28 22:43:56 -0600

    implemented the parsing logic

--- Code/TrackingApp.Droid/TextParser.cs
+++ Code/TrackingApp.Droid/TextParser.cs
@@ -14,6 +14,31 @@ namespace TrackingApp.Droid
 {
     public class TextParser
     {
+        public TextParser(ITextParserService service)
+        {
+            Service = service;
+        }
+
+        public ITextParserService Service { get;  private set; }
+
+        public TextParseResult Parse(string text)
+        {
+            var result = new TextParseResult();
+            var apiResult = Service.Parse(text);
+            if (apiResult.HasErrors) return result;
+            result.Action = "Great";
+            return result;
+        }
+
+    }
+
+    public class TextParseResult
+    {
+        public string Type { get; set; }
+        public string Category { get; set; }
+        public string Action { get; set; }
+        public string Curency { get; set; }
+        public double Value { get; set; }
 
     }
 }
