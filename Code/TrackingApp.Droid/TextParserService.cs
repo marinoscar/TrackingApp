91d42fd92db597f71285a006820303efb0e375a9
Oscar Marin <oscar@marin.cr>
2016-04-28 22:43:32 -0600

    implemented the service request

--- Code/TrackingApp.Droid/TextParserService.cs
+++ Code/TrackingApp.Droid/TextParserService.cs
@@ -9,12 +9,41 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using RestSharp;
+using Android.Content.Res;
 
 namespace TrackingApp.Droid
 {
-    public class TextParserService
+
+    public interface ITextParserService
     {
-        
+        ApiResult<TextParserServiceResult> Parse(string text);
+    }
+
+    public class TextParserService : ITextParserService
+    {
+        public ApiResult<TextParserServiceResult> Parse(string text)
+        {
+            var dic = new Dictionary<string, string>()
+            {
+                {"url",  Resources.System.GetString(Resource.String.LuisEndPoint)},
+                {"id",  Resources.System.GetString(Resource.String.LuisId)},
+                {"key",  Resources.System.GetString(Resource.String.LuisKey)},
+            };
+            var client = new RestClient(dic["url"]);
+            var request = GetRequest(dic, text);
+            var requestor = new ApiRequestor();
+            return requestor.Execute<TextParserServiceResult>(client, request);
+        }
+
+        private IRestRequest GetRequest(Dictionary<string, string> dic, string text)
+        {
+            var request = new RestRequest(string.Format("{0}/predict", dic["id"]), Method.POST);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddHeader("Ocp-Apim-Subscription-Key", dic["key"]);
+            request.AddJsonBody(new string[] { text });
+            return request;
+        }
     }
 
     public class TextParserServiceResult
