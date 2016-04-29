71583a4b805f635a3300530b35683fa032b48b7e
Oscar Marin <oscar@marin.cr>
2016-04-28 20:52:30 -0600

    added code to handle speech recognition

--- Code/TrackingApp.Droid/MainPresenter.cs
+++ Code/TrackingApp.Droid/MainPresenter.cs
@@ -9,6 +9,7 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Android.Speech;
 
 namespace TrackingApp.Droid
 {
@@ -23,14 +24,32 @@ namespace TrackingApp.Droid
 
         public override void BindView()
         {
-            var button = Activity.FindViewById<Button>(Resource.Id.MyButton);
-            button.Click += OnButtonClick;
+            Activity.FindViewById<Button>(Resource.Id.MyButton).Click += OnBasicButtonClick;
+            Activity.FindViewById<Button>(Resource.Id.SpeechButton).Click += OnSpeechButtonClick;
+            Activity.ActivityResult += OnActivityResult;
         }
 
-        private void OnButtonClick(object sender, EventArgs e)
+        private void OnActivityResult(object sender, ActivityResultArgs e)
+        {
+            if (e.RequestCode != (int)Request.Voice) return;
+            if (e.ResultCode != Result.Ok) return;
+            var results = e.Data.GetStringArrayListExtra(RecognizerIntent.ExtraResults);
+            if (!results.Any()) return;
+            var result = string.Join(" ", results);
+        }
+
+        private void OnBasicButtonClick(object sender, EventArgs e)
         {
             var button = (Button)sender;
             button.Text = string.Format("{0} clicks!", _count++);
+        }
+
+        private void OnSpeechButtonClick(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            if (!Validator.MicCheck(button.Context)) return;
+            var voice = new VoiceRecognition(Activity);
+            voice.Start();
         }
     }
 }
