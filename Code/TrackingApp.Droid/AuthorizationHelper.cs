60b752d023ab2a56502247faa42bdb989d46957a
Oscar Marin <oscar@marin.cr>
2016-04-30 13:22:04 -0600

    changed how the authorization headers are processed

--- Code/TrackingApp.Droid/AuthorizationHelper.cs
+++ Code/TrackingApp.Droid/AuthorizationHelper.cs
@@ -27,14 +27,14 @@ namespace TrackingApp.Droid
         public string Account { get; private set; }
         public string Key { get; private set; }
 
-        public string Get(Method method, DateTime time, string resource)
+        public string Get(Method method, DateTime time, string tableName)
         {
             var methodName = Enum.GetName(typeof(Method), method).ToUpperInvariant();
             var signature = string.Format("{0}\n\n{1}\n{2}\n{3}",
                     methodName,
                     "application/json",
-                    time.ToString("R", System.Globalization.CultureInfo.InvariantCulture),
-                    GetCanonicalizedResource(resource, Account)
+                    time.ToHeader(),
+                    GetCanonicalizedResource(tableName)
                     );
             var header = "SharedKey {0}:{1}";
             var bytes = Encoding.UTF8.GetBytes(signature);
@@ -45,11 +45,11 @@ namespace TrackingApp.Droid
             return header;
         }
 
-        private string GetCanonicalizedResource(string resource, string accountName)
+        private string GetCanonicalizedResource(string resource)
         {
             var sanitized = resource;
             if (sanitized.StartsWith("/")) sanitized = sanitized.Remove(0, 1);
-            return string.Format("/{0}/{1}", accountName, sanitized);
+            return string.Format("/{0}/{1}", Account, sanitized);
         }
     }
 }
