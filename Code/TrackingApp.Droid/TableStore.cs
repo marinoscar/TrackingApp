c1e58d54bd35c939b3571768746b483de6ad68e8
Oscar Marin <oscar@marin.cr>
2016-04-29 21:20:21 -0600

    added new methods to the store

--- Code/TrackingApp.Droid/TableStore.cs
+++ Code/TrackingApp.Droid/TableStore.cs
@@ -16,7 +16,7 @@ using System.Threading.Tasks;
 
 namespace TrackingApp.Droid
 {
-    public class TableStore<T> where T : TableEntity
+    public class TableStore<T> : ITableStore<T>, ITableStore<T> where T : TableEntity
     {
 
         private enum Action { None, Add, Edit, Delete }
@@ -39,6 +39,48 @@ namespace TrackingApp.Droid
         public void Add(T item)
         {
             _items.Add(new Tuple<T, Action>(item, Action.Add));
+        }
+
+        public Task<TableResult> AddAndPersistAsync(T item)
+        {
+            return ExecuteOperationAsync(TableOperation.Insert(item));
+        }
+
+        public TableResult AddAndPersist(T item)
+        {
+            return ExecuteOperation(TableOperation.Insert(item));
+        }
+
+        public Task<TableResult> EditAndPersistAsync(T item)
+        {
+            return ExecuteOperationAsync(TableOperation.InsertOrReplace(item));
+        }
+
+        public TableResult EditAndPersist(T item)
+        {
+            return ExecuteOperation(TableOperation.InsertOrReplace(item));
+        }
+
+        public Task<TableResult> DeleteAndPersistAsync(T item)
+        {
+            return ExecuteOperationAsync(TableOperation.Delete(item));
+        }
+
+        public TableResult DeleteAndPersist(T item)
+        {
+            return ExecuteOperation(TableOperation.Delete(item));
+        }
+
+        private Task<TableResult> ExecuteOperationAsync(TableOperation operation)
+        {
+            return Table.ExecuteAsync(operation);
+        }
+
+        private TableResult ExecuteOperation(TableOperation operation)
+        {
+            var result = ExecuteOperationAsync(operation);
+            result.Wait();
+            return result.Result;
         }
 
         public void Edit(T item)
