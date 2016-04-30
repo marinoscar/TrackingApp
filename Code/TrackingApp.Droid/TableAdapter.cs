e1e370289ecd415b369ce54b9676614fee1aec00
Oscar Marin <oscar@marin.cr>
2016-04-30 13:21:48 -0600

    included missing headers

--- Code/TrackingApp.Droid/TableAdapter.cs
+++ Code/TrackingApp.Droid/TableAdapter.cs
@@ -11,6 +11,7 @@ using Android.Views;
 using Android.Widget;
 using RestSharp;
 using Newtonsoft.Json.Linq;
+using System.Net;
 
 namespace TrackingApp.Droid
 {
@@ -32,13 +33,13 @@ namespace TrackingApp.Droid
         public ApiResult<object> Add(IEntity entity)
         {
             var apiCaller = new ApiRequestor();
-            return apiCaller.Execute<object>(GetClient(), GetRequest(entity));
+            return apiCaller.Execute<object>(GetClient(), GetRequest(entity), HttpStatusCode.Created, null);
         }
 
 
         private IRestClient GetClient()
         {
-            return new RestClient(string.Format("https://{0}/.table.core.windows.net", Settings["TableStore"]));
+            return new RestClient(string.Format("https://{0}.table.core.windows.net", Settings["TableStore"]));
         }
 
         private IRestRequest GetRequest(IEntity entity)
@@ -48,10 +49,12 @@ namespace TrackingApp.Droid
             var auth = _auth.Get(Method.POST, ts, TableName);
             var request = new RestRequest(TableName, Method.POST);
             request.AddHeader("Authorization", auth);
-            request.AddHeader("x-ms-date", ts.ToString("R"));
+            request.AddHeader("x-ms-date", ts.ToHeader());
+            request.AddHeader("x-ms-version", "2015-04-05");
             request.AddHeader("Content-Type", "application/json");
             request.AddHeader("Content-Length", body.Length.ToString());
             request.AddHeader("Accept", "application/json;odata=nometadata");
+            request.AddHeader("DataServiceVersion", "3.0;NetFx");
             request.AddParameter("application/json", body, ParameterType.RequestBody);
             return request;
         }
