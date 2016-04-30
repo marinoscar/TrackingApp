ed650d0ac4c4f53d2a326c2f975a67f2f9b39756
Oscar Marin <oscar@marin.cr>
2016-04-30 01:55:35 -0600

    added a method to properly parse the result

--- Code/TrackingApp.Droid/TextParser.cs
+++ Code/TrackingApp.Droid/TextParser.cs
@@ -23,11 +23,27 @@ namespace TrackingApp.Droid
 
         public TextParseResult Parse(string text)
         {
-            var result = new TextParseResult();
-            var apiResult = Service.Parse(text);
-            if (apiResult.HasErrors) return result;
-            result.Action = "Great";
-            return result;
+            return GetFromApiResult(Service.Parse(text));
+        }
+
+        private TextParseResult GetFromApiResult(ApiResult<TextParserServiceResult> result)
+        {
+            var value = new TextParseResult();
+            var itm = result.Result;
+            if (result.HasErrors) return null;
+            var intent = itm.IntentsResults.OrderByDescending(i => i.Score).FirstOrDefault();
+            if (!(intent != null && intent.Name == "RegisterEvent")) return null;
+            if (itm.EntitiesResults.Any(i => i.Name == "Type"))
+                value.Type = itm.EntitiesResults.FirstOrDefault(i => i.Name == "Type").Word;
+            if (itm.EntitiesResults.Any(i => i.Name == "Category"))
+                value.Category = itm.EntitiesResults.FirstOrDefault(i => i.Name == "Category").Word;
+            if (itm.EntitiesResults.Any(i => i.Name == "Action"))
+                value.Action = itm.EntitiesResults.FirstOrDefault(i => i.Name == "Action").Word;
+            if (itm.EntitiesResults.Any(i => i.Name == "number"))
+                value.Value = Convert.ToDouble(itm.EntitiesResults.FirstOrDefault(i => i.Name == "number").Word);
+            if (itm.EntitiesResults.Any(i => i.Name == "Curency"))
+                value.Curency = itm.EntitiesResults.FirstOrDefault(i => i.Name == "Curency").Word;
+            return value;
         }
 
     }
