0806e56edece4373637f53d83362ace71c8c8ffe
Oscar Marin <oscar@marin.cr>
2016-05-01 17:37:20 -0600

    added a new interface

---
+++ Code/TrackingApp.Library/Configuration/IStringSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackingApp.Library.Interfaces
+{
+    public interface IStringSettings
+    {
+        string this[string settingName] { get; }
+    }
+}
